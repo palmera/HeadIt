78c00e0ebc61c4664c02607bd236503300fddd3b
palmer <alegru@gmail.com>
2018-05-02 19:48:46 -0300

    Adds background image to main menu

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -1,11 +1,14 @@
 package src.states;
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.FlxSprite;
 import flixel.ui.FlxButton;
+import openfl.Assets;
 
 class MenuState extends FlxState
 {
 
+	private var background:FlxSprite;
 	public function new()
 	{
 		super();
@@ -13,6 +16,12 @@ class MenuState extends FlxState
 
 	override function create():Void
 	{
+		super.create();
+		
+		background = new FlxSprite();
+		background.loadGraphic(Assets.getBitmapData('img/beach-hd.png'));
+		add(background);
+
 		var init_x:Int = Math.floor(FlxG.width / 2 - 40);
 
 		var btn_new = new FlxButton(init_x, 50, "New game", onNew);
